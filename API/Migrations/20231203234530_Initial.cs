db793f6a2d3546e45531c265d49830705832b0f0
henrychris <64085210+henrychris@users.noreply.github.com>
2023-12-04 00:52:25 +0100

    add migrations

---
+++ API/Migrations/20231203234530_Initial.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "CinemaDb");
+
+            migrationBuilder.CreateTable(
+                name: "AspNetRoles",
+                schema: "CinemaDb",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    NormalizedName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetRoles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUsers",
+                schema: "CinemaDb",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "character varying(450)", maxLength: 450, nullable: false),
+                    FirstName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Role = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    UserName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    NormalizedUserName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    Email = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    NormalizedEmail = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    EmailConfirmed = table.Column<bool>(type: "boolean", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: true),
+                    SecurityStamp = table.Column<string>(type: "text", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(type: "boolean", nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(type: "boolean", nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    LockoutEnabled = table.Column<bool>(type: "boolean", nullable: false),
+                    AccessFailedCount = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                schema: "CinemaDb",
+                columns: table => new
+                {
+                    MovieId = table.Column<string>(type: "character varying(450)", maxLength: 450, nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    DurationInMinutes = table.Column<int>(type: "integer", nullable: false),
+                    Genres = table.Column<string[]>(type: "text[]", nullable: false),
+                    ReleaseDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.MovieId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Theaters",
+                schema: "CinemaDb",
+                columns: table => new
+                {
+                    TheaterId = table.Column<string>(type: "character varying(450)", maxLength: 450, nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    ScreenType = table.Column<string>(type: "text", nullable: false),
+                    AvailableSeats = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Theaters", x => x.TheaterId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetRoleClaims",
+                schema: "CinemaDb",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    RoleId = table.Column<string>(type: "text", nullable: false),
+                    ClaimType = table.Column<string>(type: "text", nullable: true),
+                    ClaimValue = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetRoleClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AspNetRoleClaims_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalSchema: "CinemaDb",
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserClaims",
+                schema: "CinemaDb",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<string>(type: "character varying(450)", nullable: false),
+                    ClaimType = table.Column<string>(type: "text", nullable: true),
+                    ClaimValue = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AspNetUserClaims_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "CinemaDb",
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserLogins",
+                schema: "CinemaDb",
+                columns: table => new
+                {
+                    LoginProvider = table.Column<string>(type: "text", nullable: false),
+                    ProviderKey = table.Column<string>(type: "text", nullable: false),
+                    ProviderDisplayName = table.Column<string>(type: "text", nullable: true),
+                    UserId = table.Column<string>(type: "character varying(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserLogins", x => new { x.LoginProvider, x.ProviderKey });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserLogins_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "CinemaDb",
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserRoles",
+                schema: "CinemaDb",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "character varying(450)", nullable: false),
+                    RoleId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserRoles", x => new { x.UserId, x.RoleId });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserRoles_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalSchema: "CinemaDb",
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AspNetUserRoles_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "CinemaDb",
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserTokens",
+                schema: "CinemaDb",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "character varying(450)", nullable: false),
+                    LoginProvider = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Value = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserTokens", x => new { x.UserId, x.LoginProvider, x.Name });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "CinemaDb",
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShowTimes",
+                schema: "CinemaDb",
+                columns: table => new
+                {
+                    ShowtimeId = table.Column<string>(type: "character varying(450)", maxLength: 450, nullable: false),
+                    ScreeningDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    MovieId = table.Column<string>(type: "character varying(450)", maxLength: 450, nullable: false),
+                    TheaterId = table.Column<string>(type: "character varying(450)", nullable: true),
+                    TheatreId = table.Column<string>(type: "character varying(450)", maxLength: 450, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShowTimes", x => x.ShowtimeId);
+                    table.ForeignKey(
+                        name: "FK_ShowTimes_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalSchema: "CinemaDb",
+                        principalTable: "Movies",
+                        principalColumn: "MovieId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ShowTimes_Theaters_TheaterId",
+                        column: x => x.TheaterId,
+                        principalSchema: "CinemaDb",
+                        principalTable: "Theaters",
+                        principalColumn: "TheaterId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Seats",
+                schema: "CinemaDb",
+                columns: table => new
+                {
+                    SeatId = table.Column<string>(type: "character varying(450)", maxLength: 450, nullable: false),
+                    IsSeatReserved = table.Column<bool>(type: "boolean", nullable: false),
+                    SeatNumber = table.Column<string>(type: "text", nullable: false),
+                    TheaterId = table.Column<string>(type: "character varying(450)", nullable: true),
+                    TheatreId = table.Column<string>(type: "character varying(450)", maxLength: 450, nullable: false),
+                    ShowtimeId = table.Column<string>(type: "character varying(450)", maxLength: 450, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Seats", x => x.SeatId);
+                    table.ForeignKey(
+                        name: "FK_Seats_ShowTimes_ShowtimeId",
+                        column: x => x.ShowtimeId,
+                        principalSchema: "CinemaDb",
+                        principalTable: "ShowTimes",
+                        principalColumn: "ShowtimeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Seats_Theaters_TheaterId",
+                        column: x => x.TheaterId,
+                        principalSchema: "CinemaDb",
+                        principalTable: "Theaters",
+                        principalColumn: "TheaterId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetRoleClaims_RoleId",
+                schema: "CinemaDb",
+                table: "AspNetRoleClaims",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                schema: "CinemaDb",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserClaims_UserId",
+                schema: "CinemaDb",
+                table: "AspNetUserClaims",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserLogins_UserId",
+                schema: "CinemaDb",
+                table: "AspNetUserLogins",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_RoleId",
+                schema: "CinemaDb",
+                table: "AspNetUserRoles",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "EmailIndex",
+                schema: "CinemaDb",
+                table: "AspNetUsers",
+                column: "NormalizedEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                schema: "CinemaDb",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seats_ShowtimeId",
+                schema: "CinemaDb",
+                table: "Seats",
+                column: "ShowtimeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seats_TheaterId",
+                schema: "CinemaDb",
+                table: "Seats",
+                column: "TheaterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShowTimes_MovieId",
+                schema: "CinemaDb",
+                table: "ShowTimes",
+                column: "MovieId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShowTimes_TheaterId",
+                schema: "CinemaDb",
+                table: "ShowTimes",
+                column: "TheaterId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AspNetRoleClaims",
+                schema: "CinemaDb");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserClaims",
+                schema: "CinemaDb");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserLogins",
+                schema: "CinemaDb");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserRoles",
+                schema: "CinemaDb");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserTokens",
+                schema: "CinemaDb");
+
+            migrationBuilder.DropTable(
+                name: "Seats",
+                schema: "CinemaDb");
+
+            migrationBuilder.DropTable(
+                name: "AspNetRoles",
+                schema: "CinemaDb");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUsers",
+                schema: "CinemaDb");
+
+            migrationBuilder.DropTable(
+                name: "ShowTimes",
+                schema: "CinemaDb");
+
+            migrationBuilder.DropTable(
+                name: "Movies",
+                schema: "CinemaDb");
+
+            migrationBuilder.DropTable(
+                name: "Theaters",
+                schema: "CinemaDb");
+        }
+    }
+}
