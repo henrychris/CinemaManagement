5c24ad78207362a5ddfc77c0b862758b54736a80
henrychris <64085210+henrychris@users.noreply.github.com>
2023-12-05 00:18:36 +0100

    default to null to mark other properties as optional

--- API/Features/Movies/Requests/UpdateMovieRequest.cs
+++ API/Features/Movies/Requests/UpdateMovieRequest.cs
@@ -5,10 +5,10 @@ namespace API.Features.Movies.Requests;
 
 public record UpdateMovieRequest(
     string MovieId,
-    string? Title,
-    string? Description,
-    int? DurationInMinutes,
-    DateTime? ReleaseDate,
-    string[]? Genres,
-    int? Rating,
-    string? Director) : IRequest<ErrorOr<Updated>>;
+    string? Title = null,
+    string? Description = null,
+    int? DurationInMinutes = null,
+    DateTime? ReleaseDate = null,
+    string[]? Genres = null,
+    int? Rating = null,
+    string? Director = null) : IRequest<ErrorOr<Updated>>;
