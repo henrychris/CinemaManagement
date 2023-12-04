2ca724278e09c63f6480a4e6749c756c22c0711c
henrychris <64085210+henrychris@users.noreply.github.com>
2023-12-04 20:04:52 +0100

    Create Errors.Movie.cs

---
+++ Shared/ServiceErrors/Errors.Movie.cs
@@ -0,0 +1,17 @@
+﻿using ErrorOr;
+
+namespace Shared.ServiceErrors;
+
+public static partial class Errors
+{
+    public static class Movie
+    {
+        public static Error NotFound => Error.NotFound(
+            code: "Movie.NotFound",
+            description: "Movie not found.");
+        
+        public static Error MovieAlreadyExists => Error.Conflict(
+            code: "Movie.MovieAlreadyExists",
+            description: "This movie might already exist. Please check to be sure.");
+    }
+}
