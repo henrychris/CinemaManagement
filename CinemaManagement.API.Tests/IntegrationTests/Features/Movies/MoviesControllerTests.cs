ea9e5e81e700fe15f842446a5831b4f5e6e49f4c
henrychris <64085210+henrychris@users.noreply.github.com>
2023-12-05 00:06:11 +0100

    add get section to create movie test

--- CinemaManagement.API.Tests/IntegrationTests/Features/Movies/MoviesControllerTests.cs
+++ CinemaManagement.API.Tests/IntegrationTests/Features/Movies/MoviesControllerTests.cs
@@ -12,33 +12,40 @@ public class MoviesControllerTests : IntegrationTest
 {
     private static readonly string[] Genres = ["Crime", "Drama", "History"];
 
+    private static readonly CreateMovieRequest CreateMovieRequest = new(
+        "Killers Of The Flower Moon",
+        "When oil is discovered in 1920s Oklahoma under Osage Nation land, the Osage people are murdered " +
+        "one by one—until the FBI steps in to unravel the mystery.",
+        206,
+        new DateTime(2023, 10, 20),
+        Genres,
+        8,
+        "Martin Scorsese"
+    );
+
     [Test]
     public async Task CreateMovie_ValidRequest_ReturnsHttpOk()
     {
         // Arrange
         await AuthenticateAsync(UserRoles.Admin);
 
-        var createMovieRequest = new CreateMovieRequest(
-            "Killers Of The Flower Moon",
-            "When oil is discovered in 1920s Oklahoma under Osage Nation land, the Osage people are murdered " +
-            "one by one—until the FBI steps in to unravel the mystery.",
-            206,
-            new DateTime(2023, 10, 20),
-            Genres,
-            8,
-            "Martin Scorsese"
-        );
-
         // Act
-        var act = await TestClient.PostAsJsonAsync("Movies", createMovieRequest);
+        var act = await TestClient.PostAsJsonAsync("Movies", CreateMovieRequest);
+        var response = await act.Content.ReadFromJsonAsync<ApiResponse<CreateMovieResponse>>();
+
+        var getAct = await TestClient.GetAsync($"Movies/{response!.Data!.MovieId}");
+        var getRes = await getAct.Content.ReadFromJsonAsync<ApiResponse<GetMovieResponse>>();
 
         // Assert
         act.EnsureSuccessStatusCode();
+        getAct.EnsureSuccessStatusCode();
         act.StatusCode.Should().Be(HttpStatusCode.Created);
 
-        var response = await act.Content.ReadFromJsonAsync<ApiResponse<CreateMovieResponse>>();
-        response!.Data.Should().NotBeNull();
+        response.Data.Should().NotBeNull();
         response.Data!.MovieId.Should().NotBeNull();
         response.Success.Should().BeTrue();
+
+        getRes!.Data!.MovieId.Should().Be(response.Data.MovieId);
+        getRes.Success.Should().BeTrue();
     }
 }
