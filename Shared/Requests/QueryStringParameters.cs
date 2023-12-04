d3d686d5aa52204a5742dac9584eab54a156ee81
henrychris <64085210+henrychris@users.noreply.github.com>
2023-12-04 20:05:07 +0100

    Create QueryStringParameters.cs

---
+++ Shared/Requests/QueryStringParameters.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared.Requests;
+
+public abstract class QueryStringParameters
+{
+    public int PageNumber { get; set; } = SearchConstants.PageNumber;
+    private int _pageSize = SearchConstants.MinPageSize;
+
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = value > SearchConstants.MaxPageSize ? SearchConstants.MaxPageSize : value;
+    }
+}
