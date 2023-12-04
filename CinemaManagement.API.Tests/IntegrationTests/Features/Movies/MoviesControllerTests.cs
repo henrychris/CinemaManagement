5c2827cdd26b36aed5d46c5bf32b87e8bbae52f2
henrychris <64085210+henrychris@users.noreply.github.com>
2023-12-05 00:48:06 +0100

    add movie tests

--- CinemaManagement.API.Tests/IntegrationTests/Features/Movies/MoviesControllerTests.cs
+++ CinemaManagement.API.Tests/IntegrationTests/Features/Movies/MoviesControllerTests.cs
@@ -48,4 +48,84 @@ public class MoviesControllerTests : IntegrationTest
         getRes!.Data!.MovieId.Should().Be(response.Data.MovieId);
         getRes.Success.Should().BeTrue();
     }
+
+    [Test]
+    public async Task UpdateMovie_MovieDoesNotExist_ReturnsHttpNotFound()
+    {
+        // Arrange
+        await AuthenticateAsync(UserRoles.Admin);
+
+        // Act
+        var act = await TestClient.PutAsJsonAsync("Movies", new UpdateMovieRequest("doesntExist-2310"));
+        var response = await act.Content.ReadFromJsonAsync<ApiErrorResponse>();
+
+        // Assert
+        act.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        response!.Success.Should().BeFalse();
+    }
+
+    [Test]
+    public async Task UpdateMovie_MovieExistsAndValidDetails_ReturnsHttpNoContent()
+    {
+        // Arrange
+        await AuthenticateAsync(UserRoles.Admin);
+        const string newTitle = "New Title";
+
+        // Act
+        var createAct = await TestClient.PostAsJsonAsync("Movies", CreateMovieRequest);
+        var createResponse = await createAct.Content.ReadFromJsonAsync<ApiResponse<CreateMovieResponse>>();
+
+        var updateAct =
+            await TestClient.PutAsJsonAsync("Movies", new UpdateMovieRequest(createResponse!.Data!.MovieId, newTitle));
+
+        // Assert
+        createAct.EnsureSuccessStatusCode();
+        createAct.StatusCode.Should().Be(HttpStatusCode.Created);
+
+        updateAct.EnsureSuccessStatusCode();
+        updateAct.StatusCode.Should().Be(HttpStatusCode.NoContent);
+
+        var getAct = await TestClient.GetAsync($"Movies/{createResponse.Data!.MovieId}");
+        var getResponse = await getAct.Content.ReadFromJsonAsync<ApiResponse<GetMovieResponse>>();
+
+        getResponse!.Data.Should().NotBeNull();
+        getResponse.Data!.Title.Should().Be(newTitle);
+        // original values should be unchanged
+        getResponse.Data.Description.Should().Be(CreateMovieRequest.Description);
+    }
+
+    [Test]
+    public async Task GetMovie_MovieDoesNotExist_ReturnsHttpNotFound()
+    {
+        // Arrange
+        await AuthenticateAsync(UserRoles.Admin);
+
+        // Act
+        var getAct = await TestClient.GetAsync($"Movies/notARealId");
+        var getRes = await getAct.Content.ReadFromJsonAsync<ApiResponse<GetMovieResponse>>();
+
+        // Assert
+        getAct.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        getRes!.Success.Should().BeFalse();
+    }
+
+    [Test]
+    public async Task GetMovie_MovieExists_ReturnsHttpOk()
+    {
+        await AuthenticateAsync(UserRoles.Admin);
+
+        // Act
+        var act = await TestClient.PostAsJsonAsync("Movies", CreateMovieRequest);
+        var response = await act.Content.ReadFromJsonAsync<ApiResponse<CreateMovieResponse>>();
+
+        // Assert
+        var getAct = await TestClient.GetAsync($"Movies/{response!.Data!.MovieId}");
+        var getRes = await getAct.Content.ReadFromJsonAsync<ApiResponse<GetMovieResponse>>();
+
+        // Assert
+        act.EnsureSuccessStatusCode();
+        getAct.EnsureSuccessStatusCode();
+        getRes!.Success.Should().BeTrue();
+        getRes.Data!.MovieId.Should().Be(response.Data.MovieId);
+    }
 }
