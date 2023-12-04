81b3587dcb33456e75be1d9500969190619f143b
henrychris <64085210+henrychris@users.noreply.github.com>
2023-12-04 20:11:37 +0100

    Create MovieMapper.cs

---
+++ API/Features/Movies/MovieMapper.cs
@@ -0,0 +1,14 @@
+﻿using API.Features.Movies.Requests;
+using API.Models.Domain;
+
+namespace API.Features.Movies;
+
+public static class MovieMapper
+{
+    public static Movie ToMovieObject(CreateMovieRequest request)
+    {
+        return new Movie(request.Title, request.Description, request.DurationInMinutes, request.ReleaseDate,
+            request.Genres,
+            request.Rating, request.Director);
+    }
+}
