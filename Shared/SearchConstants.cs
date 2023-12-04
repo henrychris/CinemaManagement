44641893effd27b4bded9f425b2358e2bff84b9c
henrychris <64085210+henrychris@users.noreply.github.com>
2023-12-04 20:05:09 +0100

    Create SearchConstants.cs

---
+++ Shared/SearchConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared;
+
+public static class SearchConstants
+{
+    public const int MinPageSize = 10;
+    public const int MaxPageSize = 50;
+    public const int PageNumber = 1;
+    public const string NotFound = "N/A";
+}
