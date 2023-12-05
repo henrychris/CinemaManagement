835b87fc19b07bee840be403cf1b3ccac091433c
henrychris <64085210+henrychris@users.noreply.github.com>
2023-12-05 15:33:32 +0100

    Create PagedResponse.cs

---
+++ Shared/Responses/PagedResponse.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Shared.Responses;
+
+public class PagedResponse<T>
+{
+    public IEnumerable<T> Items { get; set; } = null!;
+    public int CurrentPage { get; set; }
+    public int TotalPages { get; set; }
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
+    public bool HasPrevious => CurrentPage > 1;
+    public bool HasNext => CurrentPage < TotalPages;
+
+    public async Task<PagedResponse<T>> ToPagedList(IQueryable<T> source, int pageNumber, int pageSize)
+    {
+        var count = source.Count();
+        var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+
+        return new PagedResponse<T>
+        {
+            Items = items,
+            TotalCount = count,
+            PageSize = pageSize,
+            CurrentPage = pageNumber,
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize)
+        };
+    }
+}
