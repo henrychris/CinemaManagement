9757c57418607d68129f8a13f8d193bf683ce124
henrychris <64085210+henrychris@users.noreply.github.com>
2023-12-06 15:54:31 +0100

    add length limit to domain properties

--- API/Models/Domain/Theater.cs
+++ API/Models/Domain/Theater.cs
@@ -6,13 +6,13 @@ namespace API.Models.Domain;
 
 public class Theater
 {
-    // todo: name according to type, and order in DB.
-    // e.g second IMAX screen would be TH-IMAX-2
     [Key, MaxLength(DomainConstants.MaxIdLength)]
     public required string TheaterId { get; set; }
 
+    [MaxLength(DomainConstants.MaxNameLength)]
     public required string Name { get; set; }
-    public required string ScreenType { get; set; } = ScreenTypes.Standard;
+    [MaxLength(DomainConstants.MaxEnumLength)]
+    public required string ScreenType { get; set; } = ScreenTypes.Standard.ToString();
     public required int AvailableSeats { get; set; }
 
     public List<ShowTime> ShowTimes { get; set; } = [];
