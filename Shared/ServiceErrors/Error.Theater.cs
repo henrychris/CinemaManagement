00627aed901b6c44a98af0366003120b5cd25404
henrychris <64085210+henrychris@users.noreply.github.com>
2023-12-06 15:55:36 +0100

    Create Error.Theater.cs

---
+++ Shared/ServiceErrors/Error.Theater.cs
@@ -0,0 +1,17 @@
+﻿using ErrorOr;
+
+namespace Shared.ServiceErrors;
+
+public static partial class Errors
+{
+    public static class Theater
+    {
+        public static Error NotFound => Error.NotFound(
+            code: "Theater.NotFound",
+            description: "Theater not found.");
+        
+        public static Error TheaterAlreadyExists => Error.Conflict(
+            code: "Theater.TheaterAlreadyExists",
+            description: "This theater might already exist. Please check to be sure.");
+    }
+}
