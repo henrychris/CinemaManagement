2ab10fdc3e747921f1049046da786ea7493ff0db
henrychris <64085210+henrychris@users.noreply.github.com>
2023-12-07 18:35:16 +0100

    mark db sets as virtual

--- API/Data/CinemaDbContext.cs
+++ API/Data/CinemaDbContext.cs
@@ -4,16 +4,25 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
-public class CinemaDbContext(DbContextOptions<CinemaDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+public class CinemaDbContext : IdentityDbContext<ApplicationUser>
 {
+    public CinemaDbContext()
+    {
+        
+    }
+
+    public CinemaDbContext(DbContextOptions<CinemaDbContext> options) : base(options)
+    {
+    }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
         modelBuilder.HasDefaultSchema("CinemaDb");
     }
 
-    public DbSet<Movie> Movies { get; set; } = null!;
-    public DbSet<Seat> Seats { get; set; } = null!;
-    public DbSet<Screening> Screenings { get; set; } = null!;
-    public DbSet<Theater> Theaters { get; set; } = null!;
+    public virtual DbSet<Movie> Movies { get; set; } = null!;
+    public virtual DbSet<Seat> Seats { get; set; } = null!;
+    public virtual DbSet<Screening> Screenings { get; set; } = null!;
+    public virtual DbSet<Theater> Theaters { get; set; } = null!;
 }
