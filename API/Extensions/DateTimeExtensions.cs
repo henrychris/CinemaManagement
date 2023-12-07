9e4c5d3e8f4ab44c6daab3ecc06434ace33b8104
henrychris <64085210+henrychris@users.noreply.github.com>
2023-12-07 18:34:35 +0100

    Create DateTimeExtensions.cs

---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Extensions;
+
+public static class DateTimeExtensions
+{
+    public static string FormatScreeningTime(this DateTime dateTime)
+    {
+        return dateTime.ToString("dd-MMMM | HH:mm");
+    }
+}
