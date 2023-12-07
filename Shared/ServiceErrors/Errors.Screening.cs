9752e8536544770df58f131e6cbf1b2164419fb6
henrychris <64085210+henrychris@users.noreply.github.com>
2023-12-07 18:35:20 +0100

    Create Errors.Screening.cs

---
+++ Shared/ServiceErrors/Errors.Screening.cs
@@ -0,0 +1,18 @@
+﻿using ErrorOr;
+
+namespace Shared.ServiceErrors;
+
+public static partial class Errors
+{
+    public static class Screening
+    {
+        public static Error NotFound => Error.NotFound(
+            code: "Screening.NotFound",
+            description: "Screening not found.");
+
+        public static Error TimeConflict => Error.Conflict(
+            code: "Screening.TimeConflict",
+            description:
+            "This screening is conflicting with another scheduled for this time slot. Change the time slot and try again.");
+    }
+}
