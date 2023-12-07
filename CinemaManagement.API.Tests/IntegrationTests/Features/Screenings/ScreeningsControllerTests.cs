38b816fbc26084d45a89d885a5a66dfdbdd44fff
henrychris <64085210+henrychris@users.noreply.github.com>
2023-12-07 18:45:07 +0100

    test get and create screening

---
+++ CinemaManagement.API.Tests/IntegrationTests/Features/Screenings/ScreeningsControllerTests.cs
@@ -0,0 +1,316 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using API.Features.Movies.CreateMovie;
+using API.Features.Screenings;
+using API.Features.Screenings.CreateScreening;
+using API.Features.Theaters.CreateTheater;
+using API.Models.Enums;
+using FluentAssertions;
+using Shared.Responses;
+
+namespace CinemaManagement.API.Tests.IntegrationTests.Features.Screenings;
+
+public class ScreeningsControllerTests : IntegrationTest
+{
+    private static readonly string[] KotFmGenres = ["Crime", "Drama", "History"];
+    private static readonly CreateMovieRequest CreateMovieRequest = new(
+        "Killers Of The Flower Moon",
+        "When oil is discovered in 1920s Oklahoma under Osage Nation land, the Osage people are murdered " +
+        "one by one—until the FBI steps in to unravel the mystery.",
+        206,
+        new DateTime(2023, 10, 20),
+        KotFmGenres,
+        8,
+        "Martin Scorsese"
+    );
+    private readonly CreateTheaterRequest _createStandardTheaterRequest = new(ScreenTypes.Standard.ToString(), 200);
+
+    [Test]
+    public async Task CreateScreening_ValidRequestReturns201()
+    {
+        // Arrange
+        await AuthenticateAsync(UserRoles.Admin);
+
+        // Act
+        var theaterAct = await TestClient.PostAsJsonAsync("Theaters", _createStandardTheaterRequest);
+        var movieAct = await TestClient.PostAsJsonAsync("Movies", CreateMovieRequest);
+
+        var theaterRes = await theaterAct.Content.ReadFromJsonAsync<ApiResponse<CreateTheaterResponse>>();
+        var movieRes = await movieAct.Content.ReadFromJsonAsync<ApiResponse<CreateMovieResponse>>();
+
+        var screeningAct = await TestClient.PostAsJsonAsync("Screenings", new CreateScreeningRequest
+        (
+            DateTime.Now.AddHours(1),
+            theaterRes!.Data!.TheaterId,
+            movieRes!.Data!.MovieId
+        ));
+        var screeningRes = await screeningAct.Content.ReadFromJsonAsync<ApiResponse<CreateScreeningResponse>>();
+
+        // Assert
+        theaterAct.EnsureSuccessStatusCode();
+        movieAct.EnsureSuccessStatusCode();
+        screeningAct.EnsureSuccessStatusCode();
+
+        screeningRes!.Data!.ScreeningId.Should().Contain(theaterRes.Data.TheaterId);
+    }
+
+    [Test]
+    public async Task CreateScreening_WithSameStartTimeAsAnotherMovie_CollisionExists_ReturnsError()
+    {
+        // Arrange
+        await AuthenticateAsync(UserRoles.Admin);
+
+        // Act
+        var theaterAct = await TestClient.PostAsJsonAsync("Theaters", _createStandardTheaterRequest);
+        var movieAct = await TestClient.PostAsJsonAsync("Movies", CreateMovieRequest);
+
+        var theaterRes = await theaterAct.Content.ReadFromJsonAsync<ApiResponse<CreateTheaterResponse>>();
+        var movieRes = await movieAct.Content.ReadFromJsonAsync<ApiResponse<CreateMovieResponse>>();
+
+        var originalScreeningAct = await TestClient.PostAsJsonAsync("Screenings", new CreateScreeningRequest
+        (
+            DateTime.Now.AddHours(1),
+            theaterRes!.Data!.TheaterId,
+            movieRes!.Data!.MovieId
+        ));
+        var duplicateScreeningAct = await TestClient.PostAsJsonAsync("Screenings", new CreateScreeningRequest
+        (
+            DateTime.Now.AddHours(1),
+            theaterRes.Data!.TheaterId,
+            movieRes.Data!.MovieId
+        ));
+
+        // Assert
+        theaterAct.EnsureSuccessStatusCode();
+        movieAct.EnsureSuccessStatusCode();
+        originalScreeningAct.EnsureSuccessStatusCode();
+
+        duplicateScreeningAct.StatusCode.Should().Be(HttpStatusCode.Conflict);
+    }
+
+    [Test]
+    public async Task CreateScreening_WithinScreeningTimeOfAnotherMovie_CollisionExists_ReturnsError()
+    {
+        // Arrange
+        await AuthenticateAsync(UserRoles.Admin);
+
+        // Act
+        var theaterAct = await TestClient.PostAsJsonAsync("Theaters", _createStandardTheaterRequest);
+        var movieAct = await TestClient.PostAsJsonAsync("Movies", CreateMovieRequest);
+
+        var theaterRes = await theaterAct.Content.ReadFromJsonAsync<ApiResponse<CreateTheaterResponse>>();
+        var movieRes = await movieAct.Content.ReadFromJsonAsync<ApiResponse<CreateMovieResponse>>();
+
+        var originalScreeningAct = await TestClient.PostAsJsonAsync("Screenings", new CreateScreeningRequest
+        (
+            DateTime.Now.AddHours(1),
+            theaterRes!.Data!.TheaterId,
+            movieRes!.Data!.MovieId
+        ));
+        var duplicateScreeningAct = await TestClient.PostAsJsonAsync("Screenings", new CreateScreeningRequest
+        (
+            // halfway through the previous screening
+            DateTime.Now.AddHours(1).AddMinutes(CreateMovieRequest.DurationInMinutes / 2.0),
+            theaterRes.Data!.TheaterId,
+            movieRes.Data!.MovieId
+        ));
+
+        // Assert
+        theaterAct.EnsureSuccessStatusCode();
+        movieAct.EnsureSuccessStatusCode();
+        originalScreeningAct.EnsureSuccessStatusCode();
+
+        duplicateScreeningAct.StatusCode.Should().Be(HttpStatusCode.Conflict);
+    }
+
+    [Test]
+    public async Task CreateScreening_StartingWhenAnotherScreeningEnds_CollisionExists_ReturnsError()
+    {
+        // Arrange
+        await AuthenticateAsync(UserRoles.Admin);
+
+        // Act
+        var theaterAct = await TestClient.PostAsJsonAsync("Theaters", _createStandardTheaterRequest);
+        var movieAct = await TestClient.PostAsJsonAsync("Movies", CreateMovieRequest);
+
+        var theaterRes = await theaterAct.Content.ReadFromJsonAsync<ApiResponse<CreateTheaterResponse>>();
+        var movieRes = await movieAct.Content.ReadFromJsonAsync<ApiResponse<CreateMovieResponse>>();
+
+        var originalScreeningAct = await TestClient.PostAsJsonAsync("Screenings", new CreateScreeningRequest
+        (
+            DateTime.Now.AddHours(1),
+            theaterRes!.Data!.TheaterId,
+            movieRes!.Data!.MovieId
+        ));
+        var duplicateScreeningAct = await TestClient.PostAsJsonAsync("Screenings", new CreateScreeningRequest
+        (
+            // starts immediately after the previous screening
+            DateTime.Now.AddHours(1).AddMinutes(CreateMovieRequest.DurationInMinutes),
+            theaterRes.Data!.TheaterId,
+            movieRes.Data!.MovieId
+        ));
+
+        // Assert
+        theaterAct.EnsureSuccessStatusCode();
+        movieAct.EnsureSuccessStatusCode();
+        originalScreeningAct.EnsureSuccessStatusCode();
+
+        duplicateScreeningAct.StatusCode.Should().Be(HttpStatusCode.Conflict);
+    }
+
+    [Test]
+    public async Task CreateScreening_EndingWhenAnotherScreeningIsStillShowing_CollisionExists_ReturnsError()
+    {
+        // Arrange
+        await AuthenticateAsync(UserRoles.Admin);
+
+        // Act
+        var theaterAct = await TestClient.PostAsJsonAsync("Theaters", _createStandardTheaterRequest);
+        var movieAct = await TestClient.PostAsJsonAsync("Movies", CreateMovieRequest);
+
+        var theaterRes = await theaterAct.Content.ReadFromJsonAsync<ApiResponse<CreateTheaterResponse>>();
+        var movieRes = await movieAct.Content.ReadFromJsonAsync<ApiResponse<CreateMovieResponse>>();
+
+        var originalScreeningAct = await TestClient.PostAsJsonAsync("Screenings", new CreateScreeningRequest
+        (
+            DateTime.Now.AddHours(5),
+            theaterRes!.Data!.TheaterId,
+            movieRes!.Data!.MovieId
+        ));
+        var duplicateScreeningAct = await TestClient.PostAsJsonAsync("Screenings", new CreateScreeningRequest
+        (
+            // starts before the other screening
+            // ends when it is showing
+            DateTime.Now.AddHours(2),
+            theaterRes.Data!.TheaterId,
+            movieRes.Data!.MovieId
+        ));
+
+        // Assert
+        theaterAct.EnsureSuccessStatusCode();
+        movieAct.EnsureSuccessStatusCode();
+        originalScreeningAct.EnsureSuccessStatusCode();
+
+        duplicateScreeningAct.StatusCode.Should().Be(HttpStatusCode.Conflict);
+    }
+
+    [Test]
+    public async Task CreateScreening_TenMinutesAfterScreeningTimeOfAnotherMovie_Returns201()
+    {
+        // Arrange
+        await AuthenticateAsync(UserRoles.Admin);
+
+        // Act
+        var theaterAct = await TestClient.PostAsJsonAsync("Theaters", _createStandardTheaterRequest);
+        var movieAct = await TestClient.PostAsJsonAsync("Movies", CreateMovieRequest);
+
+        var theaterRes = await theaterAct.Content.ReadFromJsonAsync<ApiResponse<CreateTheaterResponse>>();
+        var movieRes = await movieAct.Content.ReadFromJsonAsync<ApiResponse<CreateMovieResponse>>();
+
+        var originalScreeningAct = await TestClient.PostAsJsonAsync("Screenings", new CreateScreeningRequest
+        (
+            DateTime.Now.AddHours(1),
+            theaterRes!.Data!.TheaterId,
+            movieRes!.Data!.MovieId
+        ));
+        var duplicateScreeningAct = await TestClient.PostAsJsonAsync("Screenings", new CreateScreeningRequest
+        (
+            // starts after the collision window after the previous screening
+            DateTime.Now.AddHours(1).AddMinutes(CreateMovieRequest.DurationInMinutes).AddMinutes(10),
+            theaterRes.Data!.TheaterId,
+            movieRes.Data!.MovieId
+        ));
+
+        // Assert
+        theaterAct.EnsureSuccessStatusCode();
+        movieAct.EnsureSuccessStatusCode();
+        originalScreeningAct.EnsureSuccessStatusCode();
+
+        duplicateScreeningAct.StatusCode.Should().Be(HttpStatusCode.Created);
+    }
+
+    [Test]
+    public async Task CreateScreening_WhereEndTimeMatchesStartTimeOfAnotherMovie_ReturnsErrors()
+    {
+        // Arrange
+        await AuthenticateAsync(UserRoles.Admin);
+
+        // Act
+        var theaterAct = await TestClient.PostAsJsonAsync("Theaters", _createStandardTheaterRequest);
+        var movieAct = await TestClient.PostAsJsonAsync("Movies", CreateMovieRequest);
+
+        var theaterRes = await theaterAct.Content.ReadFromJsonAsync<ApiResponse<CreateTheaterResponse>>();
+        var movieRes = await movieAct.Content.ReadFromJsonAsync<ApiResponse<CreateMovieResponse>>();
+
+        var originalScreeningAct = await TestClient.PostAsJsonAsync("Screenings", new CreateScreeningRequest
+        (
+            DateTime.Now.AddHours(5),
+            theaterRes!.Data!.TheaterId,
+            movieRes!.Data!.MovieId
+        ));
+        var duplicateScreeningAct = await TestClient.PostAsJsonAsync("Screenings", new CreateScreeningRequest
+        (
+            // starts before the other screening
+            // ends when it is scheduled to start
+            DateTime.Now.AddHours(5).AddMinutes(-CreateMovieRequest.DurationInMinutes),
+            theaterRes.Data!.TheaterId,
+            movieRes.Data!.MovieId
+        ));
+
+        // Assert
+        theaterAct.EnsureSuccessStatusCode();
+        movieAct.EnsureSuccessStatusCode();
+        originalScreeningAct.EnsureSuccessStatusCode();
+
+        duplicateScreeningAct.StatusCode.Should().Be(HttpStatusCode.Conflict);
+    }
+
+
+    [Test]
+    public async Task GetScreening_ScreeningExists_ReturnsHttpOk()
+    {
+        // Arrange
+        await AuthenticateAsync(UserRoles.Admin);
+
+        // Act
+        var theaterAct = await TestClient.PostAsJsonAsync("Theaters", _createStandardTheaterRequest);
+        var movieAct = await TestClient.PostAsJsonAsync("Movies", CreateMovieRequest);
+
+        var theaterRes = await theaterAct.Content.ReadFromJsonAsync<ApiResponse<CreateTheaterResponse>>();
+        var movieRes = await movieAct.Content.ReadFromJsonAsync<ApiResponse<CreateMovieResponse>>();
+
+        var screeningAct = await TestClient.PostAsJsonAsync("Screenings", new CreateScreeningRequest
+        (
+            DateTime.Now.AddHours(1),
+            theaterRes!.Data!.TheaterId,
+            movieRes!.Data!.MovieId
+        ));
+        var screeningRes = await screeningAct.Content.ReadFromJsonAsync<ApiResponse<CreateScreeningResponse>>();
+
+        var getScreeningAct = await TestClient.GetAsync($"Screenings/{screeningRes!.Data!.ScreeningId}");
+        var getScreeningRes = await getScreeningAct.Content.ReadFromJsonAsync<ApiResponse<GetScreeningResponse>>();
+
+        // Assert
+        theaterAct.EnsureSuccessStatusCode();
+        movieAct.EnsureSuccessStatusCode();
+        screeningAct.EnsureSuccessStatusCode();
+        getScreeningAct.EnsureSuccessStatusCode();
+
+        screeningRes.Data!.ScreeningId.Should().Be(getScreeningRes!.Data!.ScreeningId);
+    }
+
+    [Test]
+    public async Task GetScreening_ScreeningDoesNotExist_ReturnsHttpNotFound()
+    {
+        // Arrange
+        await AuthenticateAsync(UserRoles.Admin);
+
+        // Act
+        var getAct = await TestClient.GetAsync($"Screenings/notARealId");
+        var getRes = await getAct.Content.ReadFromJsonAsync<ApiResponse<GetScreeningResponse>>();
+
+        // Assert
+        getAct.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        getRes!.Success.Should().BeFalse();
+    }
+}
---
+++ CinemaManagement.API.Tests/UnitTests/Features/Screenings/CreateScreeningHandlerTest.cs
@@ -0,0 +1,112 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using API.Data;
+using API.Features.Screenings.CreateScreening;
+using API.Models.Domain;
+using ErrorOr;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Time.Testing;
+using Moq;
+using Shared.ServiceErrors;
+
+namespace CinemaManagement.API.Tests.UnitTests.Features.Screenings;
+
+[TestFixture]
+[SuppressMessage("ReSharper", "EntityFramework.UnsupportedServerSideFunctionCall")]
+public class CreateScreeningHandlerTest
+{
+    private const string TheaterId = "theaterId";
+    private const string MovieId = "movieId";
+    private readonly Mock<CinemaDbContext> _mockContext;
+    private readonly CreateScreeningRequestHandler _handler;
+    private readonly FakeTimeProvider _fakeTimeProvider;
+
+    /// <summary>
+    /// This needs to match the dates in the screenings below.
+    /// </summary>
+    private const string BaseDate = "2023-12-10";
+
+    /// <summary>
+    /// The screenings can be for different movies, or the same, it doesn't really matter.
+    /// The point is two screenings should not clash in a theater.
+    /// </summary>
+    private readonly List<Screening> _screenings =
+    [
+        new Screening(DateTime.Parse("2023-12-10"), "Movie1", TheaterId, DateTime.Parse("2023-12-10T10:00:00"),
+            DateTime.Parse("2023-12-10T12:00:00")),
+        new Screening(DateTime.Parse("2023-12-10"), "Movie2", TheaterId, DateTime.Parse("2023-12-10T13:00:00"),
+            DateTime.Parse("2023-12-10T15:00:00")),
+        new Screening(DateTime.Parse("2023-12-10"), "Movie3", TheaterId, DateTime.Parse("2023-12-10T11:00:00"),
+            DateTime.Parse("2023-12-10T13:30:00")),
+    ];
+
+    // [SetUp]
+    public CreateScreeningHandlerTest()
+    {
+        _mockContext = new Mock<CinemaDbContext>();
+        _fakeTimeProvider = new FakeTimeProvider();
+        Mock<ILogger<CreateScreeningRequestHandler>> mockLogger = new();
+
+        _handler = new CreateScreeningRequestHandler(
+            _mockContext.Object,
+            mockLogger.Object,
+            _fakeTimeProvider
+        );
+    }
+
+    [Test]
+    public async Task CreateScreening_ValidationFails_ReturnsErrors()
+    {
+        // Arrange
+        var createScreeningReq = new CreateScreeningRequest(DateTime.Parse(BaseDate).AddHours(-1), "theaterId", "movieId");
+        _fakeTimeProvider.SetUtcNow(DateTime.Parse(BaseDate));
+
+        // Act
+        var result = await _handler.Handle(createScreeningReq, new CancellationToken());
+
+        // Assert
+        result.IsError.Should().BeTrue();
+        result.FirstError.Type.Should().Be(ErrorType.Validation);
+    }
+
+    [Test]
+    public async Task CreateScreening_MovieIdDoesNotExist_ReturnsError()
+    {
+        // Arrange
+        Movie? movie = null;
+        _fakeTimeProvider.SetUtcNow(DateTime.Parse(BaseDate));
+
+        // there's another screening in the list that starts at the same time as this
+        var createScreeningReq = new CreateScreeningRequest(DateTime.Parse(BaseDate).AddHours(1), TheaterId, MovieId);
+        _mockContext.Setup(x => x.Movies.FindAsync(MovieId)).ReturnsAsync(movie);
+
+        // Act
+        var result = await _handler.Handle(createScreeningReq, new CancellationToken());
+
+        // Assert
+        result.IsError.Should().BeTrue();
+        result.FirstError.Should().Be(Errors.Movie.NotFound);
+    }
+
+    [Test]
+    public async Task CreateScreening_TheaterIdDoesNotExist_ReturnsError()
+    {
+        // Arrange
+        Theater? theater = null;
+        _fakeTimeProvider.SetUtcNow(DateTime.Parse(BaseDate));
+
+        var createScreeningReq = new CreateScreeningRequest(DateTime.Parse(BaseDate).AddHours(1), TheaterId, MovieId);
+        _mockContext.Setup(x => x.Movies.FindAsync(MovieId))
+            .ReturnsAsync(new Movie());
+
+        _mockContext.Setup(x => x.Theaters.FindAsync(TheaterId))
+            .ReturnsAsync(theater);
+
+        // Act
+        var result = await _handler.Handle(createScreeningReq, new CancellationToken());
+
+        // Assert
+        result.IsError.Should().BeTrue();
+        result.FirstError.Should().Be(Errors.Theater.NotFound);
+    }
+}
